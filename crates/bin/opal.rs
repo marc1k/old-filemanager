662ef0e7bb77dc385c3d0d65093de23aca2629a9
marc1 <m@marc.rs>
2020-11-04 17:04:05 -0800

    repo: update Cargo.tomls

--- crates/bin/opal.rs
+++ crates/bin/opal.rs
@@ -1,6 +1,14 @@
 use opal_lib::viewport::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let bounds = Bounds::of(Size(10, 10)).at_origin(Position(2, 2));
+    let buf = Buffer::filled_with(Cell::new("x".into()), bounds);
+
+    for p in buf.bounds.iter() {
+        println!("{:?}", p);
+    }
+
+    println!("\n{:?}", buf);
 
     Ok(())
 }
